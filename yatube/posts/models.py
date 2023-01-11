8b0f842f36b7ae752d798fc07127e70e7e23480e
Aidar <aidar_ru@mail.ru>
2023-01-11 14:47:52 +0400

    создание модели Post

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    # свойство text типа TextField
+    text = models.TextField()
+
+    # свойство pub_date типа DateTimeField, текст "date published" это заголовок
+    # поля в интерфейсе администратора. auto_now_add говорит, что при создании
+    # новой записи автоматически будет подставлено текущее время и дата
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
+
+    # свойство author типа ForeignKey, ссылка на модель User
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")

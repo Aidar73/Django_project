f47e7fee4ecffc0ee567f930ae5e5b4d2ae10499
Aidar <aidar_ru@mail.ru>
2023-01-22 15:13:06 +0400

    registration

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'posts',
+    'posts.apps.PostsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

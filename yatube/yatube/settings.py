37184a1ec6437e1355efafd40cf264e4be8d3448
Aidar <aidar_ru@mail.ru>
2023-01-22 18:04:06 +0400

    registration html

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -130,5 +130,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "/auth/login/"
+
 LOGIN_REDIRECT_URL = "index"
+
 LOGOUT_REDIRECT_URL = "index"
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")

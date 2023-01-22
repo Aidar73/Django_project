f42161580516f5131e19a6d31030aab6b438b7ad
Aidar <aidar_ru@mail.ru>
2023-01-22 18:04:06 +0400

    registration html

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -128,3 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = "/auth/login/"
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "index"

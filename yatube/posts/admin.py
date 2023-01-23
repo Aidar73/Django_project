53ea487d6bea8b5b67cb03ee5135b1a9e73ee606
Aidar <aidar_ru@mail.ru>
2023-01-23 18:47:23 +0400

    admin menu

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -12,6 +12,11 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("pub_date",)
     empty_value_display = "-пусто-"
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "description")
+    search_fields = ("title",)
+    empty_value_display = "-пусто-"
 
-admin.site.register(Group)
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,26 +5,40 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(max_length=200, verbose_name='Название группы')
+    slug = models.SlugField(unique=True, verbose_name='URL адрес/')
+    description = models.TextField(verbose_name='Описание группы')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
 
 class Post(models.Model):
     objects = None
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts", verbose_name='Автор')
-    group = models.ForeignKey('Group', on_delete=models.CASCADE, related_name="posts", blank=True, null=True, verbose_name='Группа')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
+        verbose_name='Автор'
+    )
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.CASCADE,
+        related_name="posts", blank=True,
+        null=True,
+        verbose_name='Группа'
+    )
 
     def __str__(self):
         return self.text
 
     class Meta:
         ordering = ['-pub_date']
-
-
-
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
